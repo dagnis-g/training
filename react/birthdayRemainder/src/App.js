84ffe5ea0ae491a1cf738f724b4c42a0418bd915
dagnis <dagnisgulbis1@gmail.com>
2021-10-19 19:09:35 +0300

    ...

--- react/birthdayRemainder/src/App.js
+++ react/birthdayRemainder/src/App.js
@@ -2,7 +2,27 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [users, setUsers] = useState(data);
+  const clearUsers = () => {
+    setUsers([]);
+  };
+  const getUsers = () => {
+    if (users.length === 0) {
+      setUsers(data);
+    }
+  };
+  return (
+    <main>
+      <section className="container">
+        <h3>{users.length} birthdays today</h3>
+        {users.map(user => (
+          <List key={user.id} {...user} />
+        ))}
+        <button onClick={clearUsers}>clear all</button>
+        <button onClick={getUsers}>get birthdays</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
